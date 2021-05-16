a2dac5e09cdd4f3e0cd0fbedc0f192471a20c283
kai112358 <220y.gomi228@gmail.com>
2021-05-16 18:36:54 +0900

    [AC] abc109 abc109_a

---
+++ atcoder.jp/abc109/abc109_a/Main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int> >;
+  
+int main(){
+  int a,b;
+  cin>>a>>b;
+  if((a*b)%2==0){
+    cout<<"No"<<endl;
+  }else{
+    cout<<"Yes"<<endl;
+    
+  }
+}
