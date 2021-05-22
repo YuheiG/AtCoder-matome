31fed87947587ff2d498d4e59811468e59fd2773
kai112358 <220y.gomi228@gmail.com>
2021-05-22 13:19:40 +0900

    [AC] abc166 abc166_a

---
+++ atcoder.jp/abc166/abc166_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+using Graph = vector<vector<int> >;
+
+int main(){
+	string s;
+  	cin>>s;
+	if(s=="ABC")cout<<"ARC"<<endl;
+	if(s=="ARC")cout<<"ABC"<<endl;
+}
