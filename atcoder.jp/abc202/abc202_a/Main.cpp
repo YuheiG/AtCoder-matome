9b989c73b6caee073d77735eff2c43d5f82031b5
kai112358 <220y.gomi228@gmail.com>
2021-05-22 21:01:20 +0900

    [AC] abc202 abc202_a

---
+++ atcoder.jp/abc202/abc202_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+using Graph = vector<vector<int> >;
+
+int main(){
+  int a,b,c,ans;
+  cin>>a>>b>>c;
+  ans=7-a+7-b+7-c;
+  cout<<ans<<endl;
+}
