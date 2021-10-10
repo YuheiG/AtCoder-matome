c66fbc0bf53ca27e6c7c90d81c44ecfc49c9f626
kai112358 <220y.gomi228@gmail.com>
2021-10-10 11:39:15 +0900

    [AC] abc205 abc205_a

---
+++ atcoder.jp/abc205/abc205_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using namespace std;
+using Graph = vector<vector<int> >;
+
+
+int main() {
+  double ans,a,b;
+  cin>>a>>b;
+  
+  ans=b*a/100.0;
+  cout<<ans<<endl;
+  return 0;
+  
+}
