d66073efbc014e6b25ad506446887eb937b16003
kai112358 <220y.gomi228@gmail.com>
2021-11-05 20:56:47 +0900

    [AC] abc214 abc214_a

---
+++ atcoder.jp/abc214/abc214_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ll long long
+using namespace std;
+using Graph = vector<vector<int>>;
+
+int main() {
+   int n;
+   cin>>n;
+   if(n<126){cout<<4<<endl;return 0;}
+   if(n<212){cout<<6<<endl;return 0;}
+   cout<<8<<endl;
+    
+    return 0;
+}
