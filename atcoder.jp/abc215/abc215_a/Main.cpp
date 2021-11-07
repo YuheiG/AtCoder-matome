9d31293f10ac1ba93db61ea7b6e79e7a43b59e59
kai112358 <220y.gomi228@gmail.com>
2021-11-07 16:53:38 +0900

    [AC] abc215 abc215_a

---
+++ atcoder.jp/abc215/abc215_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ll long long
+using namespace std;
+using Graph = vector<vector<int>>;
+
+int main() {
+	string s;
+	cin>>s;
+	if(s=="Hello,World!"){
+		cout<<"AC"<<endl;
+	}else{
+		cout<<"WA"<<endl;
+	}
+    return 0;
+}
