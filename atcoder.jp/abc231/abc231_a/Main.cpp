6e24789aa69021321163997c6c4cfcff8eb4c5a5
kai112358 <220y.gomi228@gmail.com>
2021-12-11 21:01:48 +0900

    [AC] abc231 abc231_a

---
+++ atcoder.jp/abc231/abc231_a/Main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ll long long
+using namespace std;
+using Graph = vector<vector<int>>;
+
+int main(){
+
+
+	int d;
+	double ans;
+	cin>>d;
+
+	ans=d*1.0/100.0;
+
+	cout<<ans<<endl;
+
+	return 0;
+}
+	
+
+
+
+
+
