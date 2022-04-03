fbbc520d582a8c7a727c2f709edec01e57836a43
kai112358 <220y.gomi228@gmail.com>
2022-04-03 18:00:52 +0900

    [AC] abc241 abc241_a

---
+++ atcoder.jp/abc241/abc241_a/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+	val count_push=3
+	var now_number=0
+	var test=0
+	val a=readLine()!!.split(" ").map{it->it.toInt()}
+
+	for(i in 1..count_push){
+		now_number=a[now_number]
+	}
+
+	for(i in 1..10000){
+		for(j in 1..10000){
+			test=a[0]
+		}
+	}
+
+	println(now_number)
+    
+}
