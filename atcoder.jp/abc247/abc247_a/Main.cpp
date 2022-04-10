23da2c8eba8294a61081c13a3255f66255e2cd09
kai112358 <220y.gomi228@gmail.com>
2022-04-10 21:03:09 +0900

    [AC] abc247 abc247_a

---
+++ atcoder.jp/abc247/abc247_a/Main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ll long long
+using namespace std;
+using Graph = vector<vector<int>>;
+
+int main(){
+
+	string st;
+
+	cin>>st;
+
+	cout<<0;
+	rep(i,3)cout<<st.at(i);
+	cout<<endl;
+
+
+
+    return 0;
+		
+}
