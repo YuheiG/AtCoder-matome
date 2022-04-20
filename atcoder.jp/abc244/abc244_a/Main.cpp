6d9763c54d1ae57a657229df09342f640e38eea8
kai112358 <220y.gomi228@gmail.com>
2022-04-20 19:45:22 +0900

    [AC] abc244 abc244_a

---
+++ atcoder.jp/abc244/abc244_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ll long long
+using namespace std;
+using Graph = vector<vector<int>>;
+
+int main(){
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+	cout<<s.at(s.size()-1)<<endl;
+	
+	return 0;		
+}
