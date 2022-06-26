f4ca4b4d9738078b44c6179b8e2ae7fa558f7bd6
kai112358 <220y.gomi228@gmail.com>
2022-06-26 14:03:51 +0900

    [AC] abc252 abc252_a

---
+++ atcoder.jp/abc252/abc252_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define ll long long
+using namespace std;
+using Graph = vector<vector<int>>;
+
+int main() {
+	
+	int n;
+	cin>>n;
+	char c=n;
+	cout<<c<<endl;
+
+	return 0;
+}
+
+
